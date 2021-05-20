635b55df3c10c50123ee77ec80b2b38d7c103353
Symion Edwards <symion.edwards@gmail.com>
2021-05-20 15:36:16 +0100

    Compress images

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Image from "next/image";
 import Layout from "../components/Layout";
+import { motion } from "framer-motion";
 
 export default function Home() {
   return <Layout hero={true}></Layout>;
